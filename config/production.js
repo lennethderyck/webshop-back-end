b9058084dd8403d9e91ceda1d17556d1454bc301
Lenneth De Ryck <lenneth.deryck@student.hogent.be>
2021-12-24 11:28:31 +0100

    Test 2.0

--- config/production.js
+++ config/production.js
@@ -16,4 +16,18 @@ module.exports = {
     limit: 100,
     offset: 0,
   },
+  auth: {
+    argon: {
+      saltLength: 16,
+      hashLength: 32,
+      timeCost: 6,
+      memoryCost: 2 ** 17,
+    },
+    jwt: {
+      secret: JWT_SECRET,
+      expirationInterval: 60 * 60 * 1000,
+      issuer: 'budget.hogent.be',
+      audience: 'budget.hogent.be',
+    },
+  },
 };
